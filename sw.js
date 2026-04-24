fbaa2ae7104dd9651c751ac2fd9da0f3e6ece4a8
Alessandro Riva <alex27riva@gmail.com>
2026-04-24 17:34:18 +0200

    feat: add collapsable sections

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'handy-v10';
+const CACHE = 'handy-v11';
 const ASSETS = ['./', './index.html', './content.json', './style.css', './manifest.json', './icon.svg'];
 
 self.addEventListener('install', event => {
